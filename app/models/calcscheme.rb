222771241d37e49b2c6cc1dd1036b3bb01d1deba
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2021-08-01 13:24:33 -0500

    Bundle Update and improve Calscheme

--- app/models/calcscheme.rb
+++ app/models/calcscheme.rb
@@ -30,8 +30,10 @@ class Calcscheme
           end
         end
       end
+      self.state="OK"
       return "OK"
     else
+      self.state="Error"
       return "Error: Type or Localization not available"
     end
     # to Write back to file
@@ -48,6 +50,7 @@ class Calcscheme
       # Scheme exists, version not. Most recent (top) version to be pulled
       recentversion=@content[scheme].first[0]
       if recentversion.nil?
+        self.state="Error"
         return "Error: No version available for selected scheme."
       else
         self.setscheme_version=(scheme + "_" + recentversion).to_s
@@ -55,16 +58,58 @@ class Calcscheme
       end
     else
       # Scheme does not exist
+      self.state="Error"
       return "Error: Scheme does not exist in this localisation."
     end
   end
   
-  def run()
-    self.result="Hallo"
+  def run(inputs)
+    # Where inputs is a hash of values
+    #inputs={}
+    #inputs["basevalue"]=1000
+    # Check whether all is good to go on Model side
+    return "Error, not prepared. Run load and set first." if self.setscheme_version.nil? or self.state=="Error"
+    # Check whether inputs have been provided as hash
+    return "Error, inputs not provided as hash." unless inputs.is_a? Hash
+    # Initiate result 
+    self.result={}
+    # Check whether inputs match expectation based on json definition and add to result hash
+    self.input.each do |i|
+      return "Error, missing input: " + i["label"].to_s if inputs[i["label"]].nil? and i["obligatory"]=="yes"
+      return "Error, input "+ i["label"].to_s + " available but not number format." unless inputs[i["label"]].is_a? Numeric or inputs[i["label"]].nil?
+      self.result[i["label"]]=inputs[i["label"]] unless inputs[i["label"]].nil?
+    end
+    
+    # Start calculation
+    sel=self.setscheme_version.split("_")
+    @content[sel[0]][sel[1]].each do |s|
+      s["part"]==1 if s["part"].blank?
+      case s["type"]
+        when "percent"
+          # A percentage of the relevant part of the base value is added to the label category if the relevant part of the base amount is within the limits
+          if (self.result[s["base"]].to_d * s["part"].to_d) >= s["from"].to_d and (self.result[s["base"]].to_d * s["part"].to_d) <= s["to"].to_d then
+            self.result[s["label"]] = self.result[s["label"]].to_d + ( s["var"].to_d * s["part"].to_d * self.result[s["base"]].to_d)
+          end
+        when "steppercent"
+          # A percentage of the limit range or the range between low limit and base value is added to the label category if the relevant part of the base amount is above or within the limits. NO consideration of the relevant part.
+          if self.result[s["base"]].to_d > s["to"].to_d then
+            self.result[s["label"]] = self.result[s["label"]].to_d + ( (s["to"].to_d-s["from"].to_d + 1) * s["var"].to_d )
+          end
+          if (self.result[s["base"]].to_d ) >= s["from"].to_d and (self.result[s["base"]].to_d ) <= s["to"].to_d then
+            self.result[s["label"]] = self.result[s["label"]].to_d + ( (self.result[s["base"]].to_d - s["from"].to_d + 1) * s["var"].to_d )
+          end
+        when "absolute"
+          # An absolute value is added to the label category if the relevant part of the base amount is within the limits
+          if (self.result[s["base"]].to_d * s["part"].to_d) >= s["from"].to_d and (self.result[s["base"]].to_d * s["part"].to_d) <= s["to"].to_d then
+            self.result[s["label"]] = self.result[s["label"]].to_d + s["var"].to_d
+          end
+      end
+    end
+    
     return "OK"
   end
   
   private
-  attr_accessor :content
+  attr_accessor :content, :state
   
 end
