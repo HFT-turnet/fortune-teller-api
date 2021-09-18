d317c6e771b08b1fc514ffcc8430efbcaeb395af
Hendrik Thörner <hendrik@thoerni.de>
2021-09-18 12:17:19 +0200

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     #resources :accounts, only: [:index, :show, :create, :update]
-    namespace :calcs do
-      match 'discount', via: :get
-      match 'tv_model', via: :get
-      match 'tv_get' , via: :post
-    end
+    #namespace :calcs do
+    #  match 'discount', via: :get
+    #  match 'tv_model', via: :get
+    #  match 'tv_get' , via: :post
+    #end
     namespace :public do
       #match 'discount', via: :get
       #match 'tv_model', via: :get
