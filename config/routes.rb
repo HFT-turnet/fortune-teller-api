e486dad63eae155672c9fd403f3c95689b65805d
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2022-08-13 09:11:58 +0200

    Swaggerfile

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     # If this deviating route is in namespace, then
     get 'public/timeslice', to: 'public#get_timeslice', format: :json
     get 'public/summary_report', to: 'public#get_envelope', format: :json
-    namespace :public, defaults: { format: :json }  do
-      post 'summary_report', to: 'summary_report'
+    namespace :public do
+      # This namespace ignores the "to" element and looks for target within the controller with the exact name, therefore deviating functions are called above.
+      match 'summary_report', to: 'summary_report', via: :post
       match 'timeslice' , via: :post
       #match 'timemorph' , via: :post
       #match 'timeslice_sample2', via: :get
@@ -16,6 +17,10 @@ Rails.application.routes.draw do
       #match 'valueflow_complete', via: :post
       #match 'summary_report', via: :post
     end
+    namespace :beta, defaults: { format: :json }  do
+      #match 'timeslice', to: 'public#get_timeslice', via: :get
+    end
+
   end
   
 end
