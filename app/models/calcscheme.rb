01e06dc29bf97e1f6164c29865919c43afd0655d
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2023-07-18 23:58:24 +0200

    Update calcscheme.rb
    Include Scheme Manipulation on Model level

--- app/models/calcscheme.rb
+++ app/models/calcscheme.rb
@@ -1,6 +1,6 @@
 class Calcscheme 
   include ActiveModel::Model
-  attr_accessor :tags, :input, :schemes_versions, :setscheme_version, :scheme, :result, :debuglog, :countrycode #, :content 
+  attr_accessor :tags, :input, :output, :metadata, :schemelines, :schemes_versions, :setscheme_version, :scheme, :result, :debuglog, :countrycode #, :content 
   
   # General approach. load => check existence of file and prepare scheme selection. set => set scheme to be used or pull alternatives. run => apply scheme to values given
   # ESt: zu versteuerndes Einkommen: zve
@@ -38,7 +38,7 @@ class Calcscheme
       # Initiate collections
       self.tags=[]
       self.schemes_versions=[]
-      
+
       # Read all tags
       @content.each do |s|
         self.tags << s[0]
@@ -53,6 +53,7 @@ class Calcscheme
           end
         end
       end
+      
       self.state="OK"
       return "OK"
     else
@@ -72,6 +73,8 @@ class Calcscheme
     if self.schemes_versions.include? (scheme + "_" + version).to_s then
       # Direct selection of Scheme and Version
       self.setscheme_version=(scheme + "_" + version).to_s
+      # Call Outputgenerator
+      self.pulloutput
       return "OK"
     elsif self.tags.include? scheme
       # Scheme exists, version not. Most recent (top) version to be pulled
@@ -81,6 +84,8 @@ class Calcscheme
         return "Error: No version available for selected scheme."
       else
         self.setscheme_version=(scheme + "_" + recentversion).to_s
+        # Call Outputgenerator
+        self.pulloutput
         return "Corrected: Scheme exists but version had to be set to "+ recentversion +"."
       end
     else
@@ -221,6 +226,7 @@ class Calcscheme
   # SET scheme
   # Once loaded, setting works with standard as above (set)
   
+  # TEST ONLY
   def meta_trialrun(inputs)
     # Input is a simple hash with the provided data, but it is enhanced after each step with the results from the scheme that has been run.
     # Start calculation by iterating over the items in the selected scheme
@@ -240,22 +246,47 @@ class Calcscheme
       puts s
     end
   end
+  # TEST ONLY END
   
     # Go through schemes and identify input and output variables
   # Put them in sequence to identify gaps and assumptions to be set if a variable is missing
   # Output in a console format to allow Reading it on screen
-  def meta_listinputs
+  def meta_consistency(detail)
     # Switch console output on and off
     #temporary
     self.meta_load("tax","DE")
     self.set("income","2021")
     # Check whether all is good to go on Model side
     return "Error, not prepared. Run load and set first." if self.setscheme_version.nil? or self.state=="Error"
+    # Instantiate input_types
+    input_obligatory=[]
+    input_other=[]
+    input_sequence=[]
     # Go through schemes to fetch input factors
-    
-    puts "--- Overview of Scheme and variable usage ---"
-    # Iterate over entries in schemes
-    
+    sel=self.setscheme_version.split("_")
+    @content[sel[0]][sel[1]].each_with_index do |s,index|
+      subscheme=Calcscheme.new
+      subscheme.load(s["scheme"], self.countrycode)
+      # Set Scheme
+      subscheme.set(s["type"], s["version"])
+      input_sequence << (self.countrycode + "|" + s["scheme"] + "|" + s["type"] + "|" + s["version"])
+      subscheme.input.each do |i|
+        input_obligatory << i["label"] if i["obligatory"]=="yes"
+        input_other << i["label"] if i["obligatory"]=="no"
+        input_sequence << "Requires: " + i["label"] if i["obligatory"]=="yes"
+        input_sequence << "Found in: " + i["label"] if i["obligatory"]=="yes"
+      end
+    end
+    if detail=="x" 
+      puts "--- INPUT Obligatory ---"
+      puts input_obligatory.uniq
+      puts "--- INPUT Possible ---"
+      puts input_other.uniq
+      puts "--- Overview of Scheme and variable usage ---"
+      # Iterate over entries in schemes
+      puts input_sequence
+    end
+    # In the end the list of required fields must remain
     #Example
     puts "Scheme 1 receives"
     puts "Obligatory: zve, einkommen, tax"
@@ -280,23 +311,167 @@ class Calcscheme
     puts "List all Inputs from above, that are listed as assumptions"
   end
   
-  def meta_run
-    # Ensure the variable hash includes all needed variables
-    # Post those variables that will be assumed
-    
-    # Run the scheme and provide output of each step with
-    # Each subscheme to be called individually
-    # prefix: scheme and then the variable
+  def meta_inputs_needed
+    # List array with must-inputs
+  end
+  
+  def meta_run(inputs)
+    # Inputs is a simple hash with the provided data, but it is enhanced after each step with the results from the scheme that has been run.
+    # Check whether all is good to go on Model side
+    return "Error, not prepared. Run load and set first." if self.setscheme_version.nil? or self.state=="Error"
+    # Start calculation by iterating over the items in the selected scheme
+    sel=self.setscheme_version.split("_")
+    @content[sel[0]][sel[1]].each_with_index do |s,index|
+      # Load Scheme
+      subscheme=Calcscheme.new
+      subscheme.load(s["scheme"], self.countrycode)
+      # Set Scheme
+      subscheme.set(s["type"], s["version"])
+      # Run Scheme
+      subscheme.run(inputs)
+      # Add Result to input
+      #puts subscheme.result
+      inputs=inputs.merge(subscheme.result)
+      #puts inputs
+      #puts s
+    end
+    @result=inputs
     return "OK"
   end
   
   # HELPERS
+  def pulloutput
+    self.output=[]
+    # Get Scheme and Version
+    sel=self.setscheme_version.split("_")
+    @content[sel[0]][sel[1]].each_with_index do |s,index|
+      self.output << s["label"]
+    end
+    self.output=self.output.uniq
+  end
+  
+  # MANIPULATE SCHEMES
+  def create_schemefile(type, countrycode)
+    filepath="jsonlib/" + countrycode + "_" + type + ".scheme.json"
+    if not File.exists?(filepath) then
+      file = File.open(filepath,"w")
+      # Create metadata and frame
+      metadata={}
+      metadata["country"]=countrycode
+      metadata["title"]=type
+      metadata["comment1"]=""
+      metadata["input"]=""
+      file.puts(JSON.pretty_generate(metadata))
+      file.close
+    end
+  end
+  def load_scheme_edit(type, countrycode, scheme, version)
+    filepath="jsonlib/" + countrycode + "_" + type + ".scheme.json"
+    if File.exists?(filepath) then
+      file = File.read(filepath)
+      @content=JSON.parse(file)
+      self.metadata={}
+      self.metadata["country"]=@content["country"]
+      self.metadata["title"] = @content["title"]
+      self.metadata["comment1"] = @content["comment1"]
+      # Obtain Input fields
+      self.input=@content["input"]
+      
+      # Initiate collections
+      self.tags=[]
+      self.schemes={}
+      self.schemes_versions=[]
+      self.schemelines=[{}]
+      
+      # Read all tags
+      @content.each do |s|
+        self.tags << s[0]
+      end
+      
+      # Get all Schemes, exclude tags that do not have a third level and the Disclaimer and Source Tags
+      self.tags.each do |t|
+        if t=="input" or t=="title" or @content[t].first[1].nil? then
+        else
+          self.schemes[t] = @content[t]
+          @content[t].each do |tt|
+            self.schemes_versions << (t + "_" + tt[0]).to_s unless (tt[0]=="Disclaimer" or tt[0]=="Source")
+          end
+        end
+      end
+      
+      # Read the specified scheme and version into schemelines
+      return "Error: Scheme and/or Version do not exist" if @content[scheme][version].nil?
+      self.setscheme_version=(scheme + "_" + version).to_s
+      @content[scheme][version].each_with_index do |entry, line|
+        self.schemelines[line]=entry
+      end
+      
+      # Fill Output-Array
+      self.pulloutput
+      
+      self.state="OK"
+      return "OK"
+    else
+      self.state="Error"
+      return "Error: Type or Localization not available"
+    end
+  end
+  def copy_scheme_version(newversion)
+    # Requires Load Scheme Edit with an existing version
+    sel=self.setscheme_version.split("_")
+    #@content[sel[0]][newversion]=@content[sel[0]][sel[1]]
+    addhash={}
+    addhash[newversion]=@content[sel[0]][sel[1]]
+    @content[sel[0]]=addhash.merge(@content[sel[0]])
+    return "OK"
+    # Needs to be saved with old version to take effect and before switching
+  end
+  def get_operands
+    return ["addition","percent","steppercent","absolute","multiply"]
+  end
+  def template_line
+    template={}
+    template["type"]=""
+    template["base"]=""
+    template["from"]=""
+    template["to"]=""
+    template["label"]=""
+    template["var"]=""
+    return template
+  end
+  def add_scheme_lines(entry, position)
+    checkreturn=check_line(entry)
+    if checkreturn=="OK"
+      self.schemelines.insert(position, entry)
+      return "OK"
+    else
+      return checkreturn
+    end
+  end 
+  def remove_scheme_lines(position)
+    self.schemelines.delete_at(position)
+  end
+  def save_scheme(type, countrycode, scheme, version)
+    filepath="jsonlib/" + countrycode + "_" + type + ".scheme.json"
+    @content["country"]=self.metadata["country"]
+    @content["title"]=self.metadata["title"]
+    @content["comment1"]=self.metadata["comment1"]
+    @content[scheme][version]=self.schemelines
+      file = File.open(filepath,"w")
+      file.puts(JSON.pretty_generate(@content))
+      file.close
+    return "OK"
+  end
   
   private
-  attr_accessor :content, :state
-  
-  def get_variables
-    
+  attr_accessor :content, :state, :schemes
+  def check_line(entry)
+    return "Error, not matching hash" if entry["type"].nil?
+    return "Error, no type" if entry["type"].blank?
+    return "Error, not a permissible type: "+entry["type"].to_s if not entry["type"].in? self.get_operands
+    return "Error, no base" if entry["base"].blank?
+    return "Error, no label" if entry["label"].blank?
+    return "OK"
   end
   
 end
