d3ecff6dee8e759d44e3031a9fd57989830e8767
Hendrik <84269538+HFT-turnet@users.noreply.github.com>
2023-08-27 21:21:05 +0200

    minor changes

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,14 +1,16 @@
 class Lookup
+	require 'csv'
 	include ActiveModel::Model
 	attr_accessor :csvparsed 
 	
+	
 	def readcsv(filename)
-		filepath="jsonlib/"+filename+"csv"
+		filepath="jsonlib/"+filename+".csv"
 		# Add an error catcher
 		filestring = File.read(filepath)
 		self.csvparsed = CSV.parse(filestring, :headers => true)
 	end
 	def find(argumenthash)
-		
+		b=a.csvparsed.select { |row| row['Geschlecht'] == 'm' && row[' Jahr']==" 2010"}
 	end
 end
